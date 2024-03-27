ee0191c98f992bddec83d4dde2121c036ebc0197
Sulaiman <suligzus@gmail.com>
2024-03-27 11:15:37 +0200

    Add subtraction

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 """
 This module demonstrates different arithmetic operations.
 """
+
 from arithmetic.add import add
 
 
 def main():
-    """The main implementation of this application.
-    """
+    """The main implementation of this application."""
     # Demonstrate addition:
     a = 1
     b = 2
     print(f"{a} + {b} = {add(a, b)}")
 
     # Demonstrate subtraction:
+    print(f"{a} - {b} = {a-b}")
 
     # Demonstrate multiplication:
-
 
     # Demonstrate division:
 
