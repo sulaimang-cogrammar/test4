1ae171aeb8c37e33b5c69484f6f3e3e498d4ec81
Sulaiman <suligzus@gmail.com>
2024-03-27 11:14:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+This module demonstrates different arithmetic operations.
+"""
+from arithmetic.add import add
+
+
+def main():
+    """The main implementation of this application.
+    """
+    # Demonstrate addition:
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
+
+    # Demonstrate subtraction:
+
+    # Demonstrate multiplication:
+
+
+    # Demonstrate division:
+
+
+if __name__ == "__main__":
+    main()
